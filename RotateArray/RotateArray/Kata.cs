b7cab88185bd23d8215060a853f3cecb1e1b654d
Radoslav Radoev <rradoev@gmail.com>
2017-01-05 08:49:54 -0800

    Rotate array challenge. Initial setup

---
+++ RotateArray/RotateArray/Kata.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RotateArray
+{
+    class Kata
+    {
+        public object[] Rotate(object[] array, int n)
+        {
+            if (n > 0)
+            {
+                // rotate array to the right
+            }
+            else if (n < 0)
+            {
+                // rotate array to the left
+            }
+            else if (n == 0)
+            {
+                // return array unchanged
+            }
+        }
+    }
+}
