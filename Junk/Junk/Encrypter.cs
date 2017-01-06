de65a473a69eb5e92aabd99deec20009d3117619
superklamer <rradoev@gmail.com>
2017-01-05 20:37:08 -0800

    Simple Encrypter

---
+++ Junk/Junk/Encrypter.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Junk
+{
+	class Encrypter
+	{
+		public static string Encrypt (string message)
+		{
+			string outputMessage = null;
+			foreach (char c in message)
+			{
+				int i = (int)c + 1;
+				outputMessage += (char)i;
+			}
+
+			return outputMessage;
+		}
+
+
+		public static string Derypt(string message)
+		{
+			string decodedMessage = null;
+			for (int i = 0; i < message.Length; i++)
+			{
+				int j = (int)message[i] - 1;
+				decodedMessage += (char)j;
+			}
+
+			return decodedMessage;
+		}
+	}
+}
--- Junk/Junk/Program.cs
+++ Junk/Junk/Program.cs
@@ -9,12 +9,7 @@ namespace Junk
 	class Program {
 		static void Main(string[] args)
 		{
-			SmallApartment apartment = new SmallApartment();
-			ApartmentPerson person = new ApartmentPerson();
-
-			person.Name = "Petar";
-			person.House = apartment;
-			person.ShowData();
+			Encrypter.Derypt(Encrypter.Encrypt("baba"));
 		}
 	}
 }
